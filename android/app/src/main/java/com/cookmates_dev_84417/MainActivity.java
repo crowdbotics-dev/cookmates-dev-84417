1bd5a648e70eb4a218ea7deed9a0701d3156dd06
Crowdbotics <engineering@crowdbotics.com>
2023-05-18 21:24:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cookmates_dev_84417/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cookmates_dev_84417;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cookmates_dev_84417";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cookmates-dev-84417",
+  url: "https://cookmates-dev-84417.botics.co",
+  api: "https://cookmates-dev-84417.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
